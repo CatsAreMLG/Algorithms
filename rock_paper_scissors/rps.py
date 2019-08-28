f35b9557bd5ee489d16a679aacff03fdde4d1c6a
CatsAreMLG <joshuadgon@gmail.com>
2019-08-27 21:28:59 -0500

    Finish rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,25 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    options = ['rock', 'paper', 'scissors']
+    outcomes = []
+
+    def find_outcome(remaining, result=[]):
+        if remaining == 0:
+            outcomes.append(result)
+            return
+        for option in options:
+            find_outcome(remaining - 1, result + [option])
+
+    find_outcome(n, [])
+    return outcomes
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
